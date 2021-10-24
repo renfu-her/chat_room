d904fd53c99265a998ecff81db6d3002bf4b11e0
Roy <cc711612@gmail.com>
2021-10-24 14:25:32 +0800

    補上bs.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
                                 </li>
 
                                 <li>
-                                    <a href="{{ url('room/lists') }}">
+                                    <a href="{{ route('room.index') }}">
                                         My RoomList
                                     </a>
                                 </li>
@@ -101,6 +101,6 @@
     </nav>
     @yield('content')
 </div>
-
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
